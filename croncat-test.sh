c4a17706c53b43820854ad4a9b2df4860f7b81b9
Rashi Abramson <roshkins@gmail.com>
2022-02-15 13:34:33 -0800

    proposals working!

---
+++ croncat-test.sh
@@ -0,0 +1,1 @@
+near call manager_v1.croncat.testnet create_task '{"contract_id": "dev-1644873016152-47449810976228","function_id": "approve_members","cadence": "*/10 * * * * *","recurring": true,"deposit": "0","gas": 240000000000000, "arguments":"eyJkYW9faWQiOiJhdXRvZGFvLnNwdXRuaWt2Mi50ZXN0bmV0In0="}' --accountId moopaloo.testnet --amount 10
