707f6e7508ed0ce67d538543ed9d212766c2580f
Rashi Abramson <roshkins@gmail.com>
2022-03-23 11:33:54 -0700

    Untested draft written

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
-use std::cmp::max;
-
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::json_types::U128;
 use near_sdk::{AccountId, env, near_bindgen, ext_contract, log};
-use near_contract_standards::non_fungible_token::enumeration::NonFungibleTokenEnumeration;
 use serde::Deserialize;
 
 near_sdk::setup_alloc!();
@@ -11,6 +9,7 @@ near_sdk::setup_alloc!();
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Daobot {
     nft_id: AccountId,
+    dao_id: AccountId,
 }
 
 #[derive(Deserialize)]
@@ -54,21 +53,32 @@ pub trait Astrodao {
      fn get_last_proposal_id(&self) -> u64;
 }
 
+#[ext_contract(ext_nft_enum)]
+pub trait NftEnumOwners {
+    fn nft_supply_for_owner(
+        &self,
+        account_id: AccountId,
+    ) -> U128;
+}
+
 
 
 // Recieve callbacks from external contract.
 #[ext_contract(ext_self)]
 trait Callbacks {
-    fn on_get_proposals(&self, dao_id: AccountId, #[callback] proposals: Vec<Proposal>);
-    fn on_get_proposal(&self, dao_id: AccountId, #[callback] proposal: Proposal);
-    fn on_get_last_proposal_id(&self,dao_id: AccountId, #[callback] last_proposal_id: u64);
+    fn on_get_proposals(&self, #[callback] proposals: Vec<Proposal>);
+    fn on_get_proposal(&self, #[callback] proposal: Proposal);
+    fn on_get_last_proposal_id(&self, #[callback] last_proposal_id: u64);
+    fn on_approve_proposal(&self, proposal_id: u64, #[callback] nft_supply: U128);
     fn proposal_approved(&self, id: u64);
+    fn proposal_rejected(&self, id: u64);
 }   
 
 impl Default for Daobot {
     fn default() -> Self {
         Self {
             nft_id: AccountId::from(""),
+            dao_id: AccountId::from(""),
         }
     }
 }
@@ -81,23 +91,24 @@ impl Daobot {
 
     pub fn approve_members(&mut self, dao_id: AccountId, nft_id: AccountId)  {
         self.nft_id = nft_id;
-        let callback = ext_self::on_get_last_proposal_id( dao_id.clone(), &env::current_account_id(), 0, env::prepaid_gas() - env::used_gas()- GAS_FOR_DAO_VIEW - GAS_MARGIN);
-        ext_astrodao::get_last_proposal_id(&dao_id, 0, GAS_FOR_DAO_VIEW).then(callback);
+        self.dao_id = dao_id;
+        let callback = ext_self::on_get_last_proposal_id (&env::current_account_id(), 0, env::prepaid_gas() - env::used_gas()- GAS_FOR_DAO_VIEW - GAS_MARGIN);
+        ext_astrodao::get_last_proposal_id(&self.dao_id,0, GAS_FOR_DAO_VIEW).then(callback);
 
     }
 
     #[private]
-    pub fn on_get_last_proposal_id(&self, dao_id: AccountId, #[callback] last_proposal_id: u64) {
+    pub fn on_get_last_proposal_id(&self, #[callback] last_proposal_id: u64) {
         
         // let callback = ext_self::on_get_proposals(dao_id.clone(),&env::current_account_id(), 0, gas_per_call);
-        let mono_callback = ext_self::on_get_proposal(dao_id.clone(), &env::current_account_id(), 0, env::prepaid_gas() - env::used_gas()- GAS_FOR_DAO_VIEW - GAS_MARGIN);
-        ext_astrodao::get_proposal(last_proposal_id-1, &dao_id, 0, GAS_FOR_DAO_VIEW).then(mono_callback);
+        let mono_callback = ext_self::on_get_proposal(&env::current_account_id(), 0, env::prepaid_gas() - env::used_gas()- GAS_FOR_DAO_VIEW - GAS_MARGIN);
+        ext_astrodao::get_proposal(last_proposal_id-1, &self.dao_id, 0, GAS_FOR_DAO_VIEW).then(mono_callback);
         // ext_astrodao::get_proposals(max(100,last_proposal_id)-100, 100, &dao_id, 0, gas_per_call*2 )
         // .then(callback);
     }
 
     #[private]
-    pub fn on_get_proposals(&self, dao_id: &near_sdk::AccountId, #[callback] proposals: Vec<Proposal>)  {
+    pub fn on_get_proposals(&self, #[callback] proposals: Vec<Proposal>)  {
         
         let mut active_proposals = proposals.iter().filter(|p| p.status == "InProgress".to_string() && match p.kind {
             ProposalKinds::AddMemberToRole { memberId: _ } => true,
@@ -107,33 +118,51 @@ impl Daobot {
         if active_proposals.peek().is_none() {
             panic!("No active proposals");
         }
-        let proposal_count = (active_proposals.count()) as u64;
+        let proposal_count = (active_proposals.clone().count()) as u64;
         
         const ESTIMATED_USED_GAS: u64 = 7e12 as u64;
         let remaining_gas = env::prepaid_gas() - env::used_gas() - ESTIMATED_USED_GAS;
         // 2 calls plus one extra for overhead
-        const CALLS_PER_LOOP: u64 = 3;
+        const CALLS_PER_LOOP: u64 = 4;
         let gas_per_call = remaining_gas / (proposal_count * CALLS_PER_LOOP);
         log!("Gas per call: {:?}", gas_per_call);
         active_proposals.for_each(|p| {  
             log!("Remaining gas: {:?}", env::prepaid_gas() - env::used_gas());
-            // let approved =
-              ext_self::proposal_approved(p.id, &env::current_account_id(),0, gas_per_call);
-            //ext_astrodao::act_proposal(*id, "VoteApprove".to_string(), &dao_id, 0, gas_per_call).then(approved);
+            let callback = ext_self::on_approve_proposal(p.id, &env::current_account_id(), 0, gas_per_call);
+            ext_nft_enum::nft_supply_for_owner(match &p.kind {
+                ProposalKinds::AddMemberToRole { memberId } => memberId.to_owned(),
+                _ => panic!("Unexpected proposal kind"),
+            }, &self.nft_id, 0, gas_per_call).then(callback);
+
         });
-      
     }
 
     #[private]
-    pub fn on_get_proposal(&self, dao_id: &near_sdk::AccountId, #[callback] proposals: Proposal)  {
+    pub fn on_approve_proposal(&self, proposal_id: u64, #[callback] nft_supply: U128) {
+        if nft_supply.0 > 0 {
+            let approved = ext_self::proposal_approved(proposal_id, &env::current_account_id(),0, GAS_FOR_DAO_CALL);
+            ext_astrodao::act_proposal(proposal_id, "VoteApprove".to_string(), &self.dao_id, 0, GAS_FOR_DAO_CALL).then(approved);
+        } else {
+            let rejected = ext_self::proposal_rejected(proposal_id, &env::current_account_id(),0, GAS_FOR_DAO_CALL);
+            ext_astrodao::act_proposal(proposal_id, "VoteReject".to_string(), &self.dao_id, 0, GAS_FOR_DAO_CALL).then(rejected);
+        }
+}
+
+    #[private]
+    pub fn on_get_proposal(&self, #[callback] proposals: Proposal)  {
 
         let proposal_id = proposals.id;
         //let cb = ext_self::proposal_approved(proposal_id, &env::current_account_id(),0, gas_per_call);
-        ext_astrodao::act_proposal(proposal_id, "VoteApprove".to_string(), &dao_id, 0, GAS_FOR_DAO_CALL);
+        ext_astrodao::act_proposal(proposal_id, "VoteApprove".to_string(), &self.dao_id, 0, GAS_FOR_DAO_CALL);
     }
 
     #[private]
     pub fn proposal_approved(&self, id: u64) {
         log!("Proposal {} approved", id);
+    }
+
+    #[private]
+    pub fn proposal_rejected(&self, id: u64) {
+        log!("Proposal {} rejected", id);
     }
 }
